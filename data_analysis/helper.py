86f74d34217377721ba2a3046a0eb3e03346099a
mredaoudi <daoudi.mreda@outlook.com>
2022-05-27 00:39:46 +0200

    27/05/22

--- data_analysis/helper.py
+++ data_analysis/helper.py
@@ -1,51 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import f1_score, accuracy_score, confusion_matrix
+from sklearn.metrics import f1_score, accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 
 
 def accuracy(df):
     acc = accuracy_score(df['label'], df['pred']) * 100
     f1 = f1_score(df['label'], df['pred'], average='macro') * 100
     print("Accuracy: {:.3f}\nMacro F1-score: {:.3f}".format(acc, f1))
-    return plot_confusion_matrix(df['label'], df['pred'], normalize=True)
+    return cmatrix(df['label'], df['pred'])
 
 
-def plot_confusion_matrix(y_true, y_pred, 
-                          classes=['neg', 'pos', 'neu'],
-                          normalize=False,
-                          cmap=plt.cm.YlOrBr):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    (Adapted from scikit-learn docs).
-    """
-    # Compute confusion matrix
-    cm = confusion_matrix(y_true, y_pred)
-
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-    fig, ax = plt.subplots()
-    im = ax.imshow(cm, interpolation='nearest', origin='lower', cmap=cmap)
-    ax.figure.colorbar(im, ax=ax)
-    # Show all ticks
-    ax.set(xticks=np.arange(cm.shape[1]),
-           yticks=np.arange(cm.shape[0]),
-           # Label with respective list entries
-           xticklabels=classes, yticklabels=classes,
-           ylabel='True label',
-           xlabel='Predicted label')
-
-    # Set alignment of tick labels
-    plt.setp(ax.get_xticklabels(), rotation=0, ha="right",
-             rotation_mode="anchor")
-
-    # Loop over data dimensions and create text annotations
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i in range(cm.shape[0]):
-        for j in range(cm.shape[1]):
-            ax.text(j, i, format(cm[i, j], fmt),
-                    ha="center", va="center",
-                    color="white" if cm[i, j] > thresh else "black")
-    return fig, ax
+def cmatrix(label, pred):
+    cm = confusion_matrix(label, pred, labels=['pos', 'neu', 'neg'])
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['pos', 'neu', 'neg'])
+    return disp.plot(cmap=plt.cm.YlOrBr)
